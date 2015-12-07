c046adaeab952afd2c21f96759090fe8880343eb
Ken Yu <ken@moxtra.com>
2015-12-07 20:22:58 +0800

    step-00

---
+++ MoxieChat/MoxieChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MoxieChat
+//
+//  Created by Ken Yu on 12/7/15.
+//  Copyright © 2015 Ken Yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
